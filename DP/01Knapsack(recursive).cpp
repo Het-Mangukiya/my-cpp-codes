4dbf31e857d5c7d073df3336da8af2ef7f51b0ab
HET MANGUKIYA <hetmangukiya9293@gmail.com>
2026-01-02 13:52:35 +0530

    DP

---
+++ DP/01Knapsack(recursive).cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int maxProfit(vector<int> wt,vector<int> val,int W,int n){
+    if(n == 0 || W == 0){
+        return 0;
+    }
+    int itemwt = wt[n-1];
+    int itemval = val[n-1];
+    if(itemwt <= W){
+        //include
+        int ans1 = maxProfit(wt,val,W - itemwt,n-1) + itemval;
+        //exclude
+        int ans2 =maxProfit(wt,val,W ,n-1);
+        return max(ans1,ans2);
+    }
+    else{
+        return maxProfit(wt,val,W ,n-1);
+    }
+    return 0;
+
+}
+int main(){
+vector<int> wt ={10,20,30,10};
+vector<int> val ={10,20,30,420};
+cout << maxProfit(wt,val,40,5);
+return 0;
+
+}
